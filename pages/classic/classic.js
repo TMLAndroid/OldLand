57e1468a1fa9b66c1e92f7f6c6bf895f8a68fdeb
唐明龙 <TMLAndroid@163.com>
2019-12-26 11:56:33 +0800

    完成电影界面

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,3 +1,4 @@
+let http = require('../../utils/http.js');
 Page({
 
   /**
@@ -11,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    http.getLatest();
   },
 
   /**
